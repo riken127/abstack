1e86fb671b60a5c494223e298babf4f209f8a5e1
riken127 <henriquenoronha05@gmail.com>
2026-03-13 21:11:12 +0000

    feat: base parser interface

--- src/parser.hxx
+++ src/parser.hxx
@@ -8,4 +8,24 @@
 class Parser
 {
 public:
-}
+    explicit Parser(const std::vector<Token>& tokens);
+
+    Ast parse();
+private:
+    const std::vector<Token>& tokens;
+    size_t current = 0;
+
+    bool is_at_end() const;
+    const Token& peek() const;
+    const Token& previous() const;
+    const Token& advance();
+
+    bool check(TokenType type) const;
+    bool match(TokenType type);
+
+    const Token& consume(TokenType type, const char* message);
+
+    TemplateDecl parse_template();
+    ServiceDecl parse_service();
+    Stage parse_stage();
+};
