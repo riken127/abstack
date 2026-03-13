5594ffd00687f1398721c8f7aee74316a91eab91
riken127 <henriquenoronha05@gmail.com>
2026-03-13 21:05:55 +0000

    feat: ast base structs

---
+++ src/ast.hxx
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+struct Stage
+{
+    std::string name;
+};
+
+struct TemplateDecl
+{
+    std::string name;
+    std::vector<std::string> params;
+    std::vector<Stage> stages;
+};
+
+struct ServiceDecl
+{
+    std::string name;  
+};
+
+struct Ast
+{
+    std::vector<TemplateDecl> templates;
+    std::vector<ServiceDecl> services;
+};
